ad2f4ef1b8fc66f40e3a17411c99b32a3c39994b
willdomkahari <developer.wadu@gmail.com>
2025-03-22 18:14:15 +0200

    FEAT: define the command

---
+++ behavioural/commandpattern/src/main/java/DatabaseCommand.java
@@ -0,0 +1,7 @@
+/**
+ * This is the contract for all database commands
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+public interface DatabaseCommand {
+    void execute();
+}
