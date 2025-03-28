9a8e6b4e6f5ef8a9a7fc5ac31f03d90024a8be49
willdomkahari <developer.wadu@gmail.com>
2025-03-28 21:32:42 +0200

    FEAT: target interface

---
+++ structural/adapter/adapter-pattern/src/main/java/MediaPlayer.java
@@ -0,0 +1,7 @@
+/**
+ * Target Interface
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+public interface MediaPlayer {
+    void play(AudioType type, String filename);
+}
