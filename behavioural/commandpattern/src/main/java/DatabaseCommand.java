909c890d27f063b2aec5b73942b0fb1ec87be273
willdomkahari <developer.wadu@gmail.com>
2025-03-31 18:12:52 +0200

    REFACTOR: organise code

--- behavioural/commandpattern/src/main/java/DatabaseCommand.java
+++
@@ -1,7 +0,0 @@
-/**
- * This is the contract for all database commands
- * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
- */
-public interface DatabaseCommand {
-    void execute();
-}
--- behavioural/commandpatternboot/src/main/java/com/waduclay/commandpatternboot/Command.java
+++
@@ -1,9 +0,0 @@
-package com.waduclay.commandpatternboot;
-
-
-/**
- * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
- */
-public interface Command {
-    void execute();
-}
